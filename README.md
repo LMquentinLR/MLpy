4f4f84e8751cc232623d1b29f9731cee56a86350
Quentin Le Roux <quentin.leroux@edhec.com>
2020-10-31 00:16:45 +0100

    gitignore repair

---
+++ README.md
@@ -0,0 +1,13 @@
+# MLpy
+
+### What is it about?
+
+**WIP**
+
+### derpi_get package
+
+**WIP**
+
+### Disclaimer
+
+Derpibooru's REST API license forbids a high load of requests per second, please keep the exponential back off functional if you try this code
---
+++ modules/pgdump_send_commands_to_psql.py
@@ -0,0 +1,61 @@
+import psycopg2 #import connect, extensions, sql
+from configparser import ConfigParser
+
+def retrieve_config_parameters(filename, section):
+    """
+    Retrieves the config parameters that specify how to connect to a database.
+    """
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+def connect_execute_disconnect_from_psql(filename, section, command_list):
+    """ 
+    Connects to the PostgreSQL database server using the config parameters available in
+    <filename>, section <section>. Executes a list of SQL commands <command_list>. Then
+    Exits.
+    """
+    # Variable initialization
+    conn = None
+    ##########
+    try:
+        # Reads connection parameters
+        params = retrieve_config_parameters(filename, section)
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+        # get the isolation leve for autocommit
+        autocommit = psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT
+        print("ISOLATION_LEVEL_AUTOCOMMIT:", psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+        # set the isolation level for the connection's cursors
+        # will raise ActiveSqlTransaction exception otherwise
+        conn.set_isolation_level( autocommit )
+        # create a cursor
+        cur = conn.cursor()
+        # execute The statements
+        for statement in command_list:
+            try:
+                print(statement)
+                cur.execute(command_list[statement])
+            except Exception as error:
+                print("Failed: " + str(error))
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
