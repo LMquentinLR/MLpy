5d5eabc1fa71f0b3238351e0f080fe1f23b6dd85
Quentin Le Roux <quentin.leroux@edhec.com>
2020-10-31 00:15:33 +0100

    .gitignore is now working v2

--- modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from .download_dump import download_most_recent_pgdump  
-from .pgdump_send_commands_to_psql import connect_execute_disconnect_from_psql, retrieve_config_parameters
-from .pgdump_restore import restore_db
-from .pgdump_save_db import save_db
